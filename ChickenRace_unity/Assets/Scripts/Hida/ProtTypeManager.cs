750d72359d7e9cab7ef2d2ee3123d44531aa480d
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2023-12-06 15:27:35 +0900

    Init手順の確認

--- ChickenRace_unity/Assets/Scripts/Hida/ProtTypeManager.cs
+++ ChickenRace_unity/Assets/Scripts/Hida/ProtTypeManager.cs
@@ -13,11 +13,14 @@ public class ProtTypeManager : MonoBehaviour
         RESULT
     }
 
+    //初期化の段階を示す
+    //その段階が終わると次の状態に進む
     enum InitStatus
     {
-        READY,
-        NOW,
-        WAIT
+        CONECT,
+        RESET,
+        WAIT,
+        START
     }
 
     [SerializeField, Tooltip("現在のゲーム状態")] GameStatus gameState;
@@ -45,16 +48,16 @@ public class ProtTypeManager : MonoBehaviour
 
     /// <summary>
     /// 各プレイヤーの初期化状態を確認する
-    /// 全員がWait状態の時にtrueを返す
+    /// 全員が引数の状態の時にtrueを返す
     /// この初期化状態はカスタムプロパティで管理する
     /// </summary>
     /// <param name="status"></param>
     /// <returns></returns>
-    bool CheckInitState()
+    bool CheckInitState(InitStatus status)
     {
         //全員の初期化状態を確認
-        InitStatus playerinitlist = InitStatus.WAIT;
-        if (playerinitlist == InitStatus.WAIT) return true;
+        InitStatus playerinitlist = status;
+        if (playerinitlist == status) return true;
         else return false;
 
     }
@@ -66,34 +69,49 @@ public class ProtTypeManager : MonoBehaviour
     IEnumerator GameInit()
     {
         //状態を送信
-        InitStatus initStatus = InitStatus.READY;
+        InitStatus initStatus = InitStatus.CONECT;
 
-        //1.接続を確認
-        //状態を送信
-        initStatus = InitStatus.NOW;
-        //フォトンの機能で接続しているか確認
-        while (false)
+        //1.接続を確認<CONECT
         {
-            //接続まで待機
-            Debug.Log("接続確認中..");
-            yield return null;
+            //フォトンの機能で接続しているか確認
+            while (false)
+            {
+                //接続まで待機
+                Debug.Log("接続確認中..");
+                yield return null;
+            }
+
+            //状態を送信
+            initStatus = InitStatus.RESET;
+            //他のプレイヤーを待機
+            yield return new WaitUntil(() => CheckInitState(InitStatus.RESET));
+            Debug.Log("接続確認!");
+
         }
+        //2.各値を初期化<RESET
+        {
+            gameState = GameStatus.READY;
 
-        //状態を送信
-        initStatus = InitStatus.WAIT;
-        //他のプレイヤーを待機
-        yield return new WaitUntil(() => CheckInitState());
+            //各マネージャーを生成
 
-        //2.各値を初期化
-        gameState = GameStatus.READY;
+            //各マネージャー初期化確認・待機
 
-        //各マネージャーを生成
+            //Playerクラス生成
 
-        //各マネージャー初期化確認・待機
-
-        //Playerクラス生成
-
-        //初期化完了・他プレイヤーを待機
+            //状態を送信
+            initStatus = InitStatus.WAIT;
+            //他のプレイヤーを待機
+            yield return new WaitUntil(() => CheckInitState(InitStatus.WAIT));
+        }
+        //3.初期化完了・他プレイヤーを待機<WAIT
+        {
+            //同時にゲーム開始
+            //状態を送信
+            initStatus = InitStatus.START;
+            //他のプレイヤーを待機
+            yield return new WaitUntil(() => CheckInitState(InitStatus.START));
+        }
+        Debug.Log("初期化完了");
     }
 
     #endregion
