f0015d865e6039d7c2828b4175a15b5395ad59d2
siitakee <r03.gm.2110016@da.iwasaki.ac.jp>
2023-12-07 10:53:57 +0900

    障害物設置関数の修正、グリッド配置

--- ChickenRace_unity/Assets/Scripts/NojiriScripts/MapManager.cs
+++ ChickenRace_unity/Assets/Scripts/NojiriScripts/MapManager.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
-    public Vector2Int gridPos = new Vector2Int(2, -3); // テスト用設置位置
+    public Vector3Int gridPos; // テスト用設置位置
 
     [SerializeField] private GameObject gameObject; // 移動したいオブジェクトの情報取得
     [SerializeField] private List<GameObject> InstalledList; // 設置した障害物リスト
     [SerializeField] private List<Vector2Int> UsedGridList;   // 使用済みグリッドの位置リスト
+    [SerializeField] private Tilemap tilemap;
 
     private bool isRunning = false; // コルーチン実行判定フラグ
     private bool isInstall = false; // 設置フラグ
@@ -33,10 +35,10 @@ public class MapManager : MonoBehaviour
             CreativeModeEnd();
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            GenerateMapObject();
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    GenerateMapObject();
+        //}
     }
 
     /// <summary>
@@ -59,17 +61,26 @@ public class MapManager : MonoBehaviour
         while (true)
         {
             /* 
-            ここで
-            ・移動メソッド
-            ・設置位置取得メソッド
-            ・障害物生成メソッド
-            を呼び出す
+            コルーチン実行中に
+            ・設置判定　　　：JudgeInstall()
+            ・障害物生成　　：GenerateMapObject()
+            ・コルーチン終了：CreativeModeEnd()
+            のメソッドを呼び出せる
             */
 
             yield return null;
         }
 
         Debug.Log("コルーチン終了");
+    }
+
+    /// <summary>
+    /// 設置位置確定メソッド
+    /// 設置が確定した場合、設置位置を送る
+    /// </summary>
+    private Vector3 ConfirmPosition()
+    {
+        return Input.mousePosition;
     }
 
     #region 外部用メソッド
@@ -121,6 +132,7 @@ public class MapManager : MonoBehaviour
         {
             if (installPos == UsedGridList[i])
             {
+                Debug.LogError("障害物が配置済みです");
                 return true;
             }
         }
@@ -133,7 +145,7 @@ public class MapManager : MonoBehaviour
     /// JudgeInstallメソッドからtrueが返った時に呼ばれる
     /// ID、グリッド位置を取得後、その位置に障害物生成
     /// </summary>
-    public void GenerateMapObject(/*id, Vector2Int gridPos*/)
+    public void GenerateMapObject(int id, Vector2Int gridPos)
     {
         if (!isRunning)
         {
@@ -141,20 +153,36 @@ public class MapManager : MonoBehaviour
             return;
         }
 
-        //Vector3 installPos; // 仮設置位置
-
-        // クリック位置取得
-        //installPos = Input.mousePosition;
-        //installPos.x = Mathf.Round(installPos.x);
-        //installPos.y = Mathf.Round(installPos.y);
-        //installPos.z = 10.0f;
-
-        //Debug.Log(installPos);
-
         // カーソルの位置に障害物を生成
-        // 生成位置のz軸が勝手に-10されるため、10に設定しておく
         GameObject gameObj = (GameObject)Resources.Load("Square"); // 仮のSquare
-        Instantiate(gameObj, new Vector3(gridPos.x, gridPos.y, 10.0f)/*Camera.main.ScreenToWorldPoint(new Vector3Int(gridPos.x, gridPos.y, 10))*/, Quaternion.identity);
+
+        // ワールド座標のマウス座標を取得
+        // Mathf.RoundToIntでVector3からVector2Intに変換予定
+        //Vector3 installPos = Camera.main.ScreenToWorldPoint(gridPos);
+        Vector3 installPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //テスト
+
+        // テスト
+        //Vector3Int installPos;
+        //gridPos = tilemap.WorldToCell(installPos);
+        //Vector3 complementPos = new Vector3(tilemap.cellSize.x / 2, tilemap.cellSize.y / 2, 0);
+        //Vector3 worldPos = tilemap.CellToWorld(gridPos) + complementPos;
+        //今選択している場所にオブジェクトを移動させる。
+        //gameObj.transform.position = worldPos;
+
+        // 座標を四捨五入、生成位置のz軸が勝手に-10されるため、0に設定しておく
+        Vector2Int installPosInt;
+        installPosInt = new Vector2Int(Mathf.RoundToInt(installPos.x), Mathf.RoundToInt(installPos.y));
+        //installPos.y = Mathf.RoundToInt(installPos.y);
+        //installPos.z = 0f;
+
+        // 障害物の生成
+        Instantiate(gameObj, new Vector3(installPosInt.x, installPosInt.y, 0f), Quaternion.identity);
+
+        Debug.Log(installPos);
+
+        // 設置したオブジェクトをリストに追加
+        InstalledList.Add(gameObj);
+        UsedGridList.Add(installPosInt);
     }
     #endregion
 }
