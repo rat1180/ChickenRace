08b7acc799741aad506e0c2c072914693222b6a5
HirotoNagatsuka <93108443+HirotoNagatsuka@users.noreply.github.com>
2023-12-14 11:47:49 +0900

    完売にする関数の作成・画像にIDを持たせた

---
+++ ChickenRace_unity/Assets/Scripts/NagatsukaScripts/ObstacleImage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ResorceNames;
+
+public class ObstacleImage : MonoBehaviour
+{
+    public OBSTACLE_IMAGE_NAMES id;
+
+    public OBSTACLE_IMAGE_NAMES ReturnID()
+    {
+        return id;
+    }
+}
