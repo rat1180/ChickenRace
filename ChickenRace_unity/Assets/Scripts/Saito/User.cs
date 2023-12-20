2b0c933d84fd9bf8431c4a147ae10fcd293090fe
sfslime <93108188+sfslime@users.noreply.github.com>
2023-12-20 14:07:37 +0900

    初期化用関数追加 ダンスアニメーション作成(🍊)

--- ChickenRace_unity/Assets/Scripts/Saito/User.cs
+++ ChickenRace_unity/Assets/Scripts/Saito/User.cs
@@ -15,9 +15,20 @@ public class User : MonoBehaviour
     GameObject mouseObjcet;
     GameObject playerObjcet;
 
+    /// <summary>
+    /// èâä˙âªópä÷êî.
+    /// </summary>
+    void Init()
+    {
+        isMode = 0;
+        modeId = 0;
+        itemId = -1;
+        index = -1;
+    }
+
     void Start()
     {
-        
+        Init();
     }
 
 
