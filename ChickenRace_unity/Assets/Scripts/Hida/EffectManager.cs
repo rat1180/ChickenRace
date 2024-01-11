48617bd161a1172d691acfbfd14b41e3bc6336a4
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2024-01-11 14:50:01 +0900

    スタート演出（未完）

---
+++ ChickenRace_unity/Assets/Scripts/Hida/EffectManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    enum AnimCode
+    {
+        None,
+        Start,
+        StartEnd,
+        Ready,
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
